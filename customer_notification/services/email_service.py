f4b4eaba5d5e46eb07dc7dba820612a589d7129b
Arturo Uribe <auribe21@Mac.lan>
2026-03-05 22:24:11 -0500

    update customer_notification

--- customer_notification/services/email_service.py
+++ customer_notification/services/email_service.py
@@ -1,18 +1,17 @@
 import boto3
-from jinja2 import Environment, FileSystemLoader
 from config import AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION, SENDER_EMAIL
+from services.template_service import render_template
 
 
 def send_payment_reminder(email, name, amount, due_date):
 
-    # Load HTML template
-    env = Environment(loader=FileSystemLoader("templates"))
-    template = env.get_template("payment_reminder.html")
-
-    html_content = template.render(
-        customer_name=name,
-        amount=amount,
-        due_date=due_date
+    html_content = render_template(
+        "payment_reminder.html",
+        {
+            "customer_name": name,
+            "amount": amount,
+            "due_date": due_date
+        }
     )
 
     ses = boto3.client(
